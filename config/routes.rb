f467a2efd975897a4e8318b0a551f449519c879c
djany082 <djany082@gmail.com>
2019-10-25 18:56:52 +0200

    Admin Dashboard working and ussers can send messages to admin. Script still failling for messaging admin

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
 
-    root to: "users#index"
+  #root to: "users#index"
   end
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
+  get '/admin-dashboard', to: 'rooms#index'
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
@@ -18,7 +19,7 @@ Rails.application.routes.draw do
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  #root to: 'home#index'
-  root controller: :rooms, action: :index
+  root to: 'home#index'
+  #root controller: :rooms, action: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
